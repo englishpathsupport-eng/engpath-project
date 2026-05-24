a0509b53b292b1b8595e14c0b782b23295bbd3b4
Arshad <englishpathsupport@gmail.com>
2026-05-24 22:39:49 +0400

    fix react import

--- src/engpath-app-secure.jsx
+++ src/engpath-app-secure.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { useState, useEffect, useRef, useReducer, useCallback, useMemo, memo } from "react";
 
 /* ═══ Audio Unlock ═══ */
